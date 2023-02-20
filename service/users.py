fac3e9380699f8bfee13d7925b577057a9c4a4c3
erick <erickedg@hotmail.com>
2023-02-20 12:21:06 -0700

    Subiendo servicios de users

---
+++ service/users.py
@@ -0,0 +1,30 @@
+from models.users import Users as UsersModel
+from schemas.users import Users
+
+class UsersSevice():
+    def get_users(self):
+        result = self.db.query(UsersModel).all()
+        return result
+
+    def get_user(self, id:int):
+        result = self.db.query(UsersModel).filter(UsersModel.iduser == id).first()
+        return result
+
+    def create_user(self):
+        new_user = UsersModel(**Users.dict())
+        self.db.add(new_user)
+        self.db.commit()
+        return
+
+    def update_user(self, id:int, data:Users):
+        user = self.db.query(UsersModel).filter(UsersModel.iduser == id).first()
+        user.name = data.name
+        user.username = data.username
+        user.status = data.status
+        self.db.commit()
+        return
+
+    def delete_user(self, id:int):
+        self.db.query(UsersModel).filter(UsersModel.iduser == id).delete()
+        self.db.commit()
+        return
