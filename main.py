8d94c2a89ff77ac29772ba29c5b82980fac9db2b
erick <erickedg@hotmail.com>
2023-03-06 10:23:45 -0700

    modificacion en main, agregando la ruta de usuarios

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
-
 #importar el archivo de base de datos
 from config.database import engine, Base
 from routers.product import product_router
+from routers.users import users_router
 
 app = FastAPI()
 app.title = "API del proyecto ProyectKCE"
 app.version = "0.0.1"
 
 app.include_router(product_router)
+app.include_router(users_router)
 
 Base.metadata.create_all(bind=engine)
